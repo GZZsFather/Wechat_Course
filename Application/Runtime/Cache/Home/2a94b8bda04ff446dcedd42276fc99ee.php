6022e842f575c77072ee216fe2c64509128498cc
GZZsFather <490906019@qq.com>
2018-02-11 23:26:33 +0800

    App_Path

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,38 @@
+<?php if (!defined('THINK_PATH')) exit();?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<link rel="stylesheet" href="/Public/css/style.css">
+
+<!--	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>-->
+
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+</head>
+<body>
+	<h1>holy shit</h1>
+	<div class="main-agileinfo">
+		<h2></h2>
+		<form action="#" method="post">
+			<input type="text" name="name" class="name" placeholder="Username" required="">
+			<input type="password" name="password" class="password" placeholder="Password" required="">
+			<ul>
+				<li>
+					<input type="checkbox" id="brand1" value="">
+					<label for="brand1"><span></span>记得我</label>
+				</li>
+			</ul>
+            <a href="#">忘记密码?
+						</a><br>
+			<div class="clear"></div>
+			<input type="submit" value="Login">
+		</form>
+	</div>
+	<div class="footer-w3l">
+		<p class="agile"> &copy; 2018 GZZsFather</a></p>
+	</div>
+</body>
+</html>
